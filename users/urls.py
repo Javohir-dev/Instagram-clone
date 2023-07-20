416e5f5626c0f1b795d6fa06fb82cc46947e4287
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-07-20 18:50:56 +0500

    send verify code again

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from .serializers import SignUpSerializer
-from .views import CreateUserView, VerifyAPIView
+from .views import CreateUserView, VerifyAPIView, GetNewVerification
 from django.urls import path
 
 
 urlpatterns = [
     path("signup/", CreateUserView.as_view()),
     path("verify/", VerifyAPIView.as_view()),
+    path("new-verify/", GetNewVerification.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,24 @@
 from django.utils.datetime_safe import datetime
-from rest_framework.exceptions import ValidationError
 from django.shortcuts import render
+
+from rest_framework.exceptions import ValidationError
 from rest_framework import permissions, status
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
+
+from shared.utility import send_email
+
 from .serializers import SignUpSerializer
-from .models import User, DONE, CODE_VERIFIES, NEW
+from .models import (
+    VIA_EMAIL,
+    CODE_VERIFIES,
+    NEW,
+    VIA_PHONE_NUMBER,
+    User,
+)
 
 
 class CreateUserView(CreateAPIView):
@@ -49,3 +59,36 @@ class VerifyAPIView(APIView):
             user.auth_status = CODE_VERIFIES
             user.save()
         return True
+
+
+class GetNewVerification(APIView):
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        self.check_verification(user)
+        if user.auth_type == VIA_EMAIL:
+            code = user.create_verify_code(VIA_EMAIL)
+            send_email(user.email, code)
+        elif user.auth_type == VIA_PHONE_NUMBER:
+            code = user.create_verify_code(VIA_PHONE_NUMBER)
+            send_email(user.phone_number, code)
+        else:
+            data = {
+                "message": "Email yoki Telefon raqamingiz xato yozilgan bo'lishi mumkin."
+            }
+            raise ValidationError(data)
+
+        return Response(
+            {
+                "success": True,
+                "message": "Tasdiqlash kodingiz qayta yuborildi.",
+            }
+        )
+
+    @staticmethod
+    def check_verification(user):
+        verifies = user.verify_codes.filter(
+            expiration_time__gte=datetime.now(), is_confirmed=False
+        )
+        if verifies.exists():
+            data = {"message": "Sizda kod mavjud, biroz kuting..."}
+            raise ValidationError(data)
