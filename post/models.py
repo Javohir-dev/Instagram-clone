b72ff90f1eb049112f895328f04b1454822f8d50
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-07-28 23:08:09 +0500

    PostLke and CommentLike

--- post/models.py
+++ post/models.py
@@ -5,7 +5,6 @@ from django.db.models import UniqueConstraint
 
 from shared.models import BaseModel
 
-
 User = get_user_model()
 
 
@@ -13,7 +12,7 @@ class Post(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     image = models.ImageField(
         upload_to="post_images",
-        validators=[FileExtensionValidator(allowed_extensions=["jpg", "jpeg", "png"])],
+        validators=[FileExtensionValidator(allowed_extensions=["jpeg", "jpg", "png"])],
     )
     caption = models.TextField(validators=[MaxLengthValidator(2000)])
 
@@ -22,18 +21,20 @@ class Post(BaseModel):
         verbose_name = "post"
         verbose_name_plural = "posts"
 
+    def __str__(self):
+        return f"{self.author} post about {self.caption}"
+
 
 class PostComment(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     comment = models.TextField()
     parent = models.ForeignKey(
-        "self",
-        on_delete=models.CASCADE,
-        related_name="child",
-        null=True,
-        blank=True,
+        "self", on_delete=models.CASCADE, related_name="child", null=True, blank=True
     )
+
+    def __str__(self):
+        return f"comment by {self.author}"
 
 
 class PostLike(BaseModel):
@@ -41,14 +42,18 @@ class PostLike(BaseModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
 
     class Meta:
-        constraints = [UniqueConstraint(fields=["author", "post"])]
+        constraints = [
+            UniqueConstraint(fields=["author", "post"], name="postLikeUnique")
+        ]
 
 
-class PostLike(BaseModel):
+class CommentLike(BaseModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.ForeignKey(
         PostComment, on_delete=models.CASCADE, related_name="likes"
     )
 
     class Meta:
-        constraints = [UniqueConstraint(fields=["author", "comment"])]
+        constraints = [
+            UniqueConstraint(fields=["author", "comment"], name="CommentLikeUnique")
+        ]
